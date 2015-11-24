4e78eb5b5a19686da3a4b75b97f39b6a959c4241
Jasmine <jasminelee@berkeley.edu>
2015-11-23 19:01:11 -0800

    background transition

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ jQuery(document).ready(function($) {
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     });
+   $('.header').animate({opacity: 0}, 0).css({'background-image': 'url(beach_copy.jpg) no-repeat'}).animate({opacity: 1}, 4000);
 });
